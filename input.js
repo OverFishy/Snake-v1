eda05d3e78f20b647e02ed0004e646ac2554f58a
ofek shtrit <ofek1997shtrit@gmail.com>
2022-06-27 13:27:30 +0300

    touch event listeners are attachet to board element rather then window, for the Iframe usability

--- input.js
+++ input.js
@@ -1,19 +1,24 @@
 let inputDirection = { x: 0, y: 0 }; // by default snake dont move
 let lastInputDirection = { x: 0, y: 0 }; // to be used as a check
+
 let startX;
 let startY;
 let endX;
 let endY;
 
+const board = document.getElementById('board-container')
+
 export function mobileMovment() {
-  window.addEventListener('touchstart', (e) => {
+  board.addEventListener('touchstart', (e) => {
     // Getting finger down details
     const touchList = e.changedTouches;
     const swipe = touchList.item(0)
     startX = swipe.screenX
     startY = swipe.screenY
+
+
   })
-  window.addEventListener('touchend', (e) => {
+  board.addEventListener('touchend', (e) => {
     // Getting finger up location
     const touchList = e.changedTouches;
     const swipe = touchList.item(0)
